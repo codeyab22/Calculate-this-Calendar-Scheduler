7ce3b5e09d35daca7355a5ddd3accc49d4ee45a0
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2020-12-17 00:37:58 -0500

    moment.js
    I am adding the moment.js

---
+++ moment.js
@@ -0,0 +1,7 @@
+//Moment's library I am using for the code
+
+var moment = require("moment")
+
+// format the current date
+var now = moment();
+console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"))
