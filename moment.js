76fac30e4c08b16e2426dcd10c56bd858106b7aa
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-01-06 16:05:16 -0500

    moment.js
    I using the moment.js to fix the error

--- moment.js
+++ moment.js
@@ -1,15 +1,5 @@
 //Moment's library I am using for the code
-var moment = require("moment")
-
-// format the current date
-var now = moment();
-console.log(now.format("dddd, MMMM Do YYYY, h:mm:ss a"))//date math example
-console.log(moment('2016-03-12 13:00:00').add(1, 'day').format('LLL'))
-
-//time math example
-console.log(moment('2016-03-12 13:00:00').add(24, 'hours').format('LLL'))
-
-// moment API
-moment
+var moment = require('moment'); // require
+moment().format(); 
 
 
